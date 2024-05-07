a816a874ebf56449868abd20dbe137c66765c5f0
rockwe535 <157516692+rockwe535@users.noreply.github.com>
2024-05-07 15:54:55 +0000

    add bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -86,13 +86,13 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            if (circle.y < 0) {  
+            if ( circle.y < 0 ) {  
                 circle.y = 0;
             }
-            if (circle.x < 0) {
+            if ( circle.x < 0 ) {
                 circle.x = 0;
             }
-            if (circle.y > canvas.height) {
+            if ( circle.y > canvas.height ) {
                 circle.y = 0;
             }
         }
